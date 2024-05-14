93a9c11cdf5a176ef38a42bc096a1c444756e0f3
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-14 10:02:29 -0400

    template.literals.js

---
+++ template.literals.js
@@ -0,0 +1,6 @@
+let nome = "Amanda Latorraca";
+let idade = "17";
+
+// Usando template literals para criar um string com variaveis
+let saudacao = `Ola, meu nome e ${nome} e eu tenho ${idade} anos.`; 
+console.log(saudacao); // Saida: "Ola, meu nome e Amanda Latorraca e eu tenho 17 anos."
