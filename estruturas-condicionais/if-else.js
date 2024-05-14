f1acc5edf686c973f43c964ff84442b5cf545f3d
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-14 09:56:57 -0400

    adicionado  estruturas-condicionais/if-else.js

---
+++ estruturas-condicionais/if-else.js
@@ -0,0 +1,7 @@
+let diaDaSemana = "quarta-feira";
+
+if(diaDaSemana === "sabado" || diaDaSemana === "domingo") {
+    console.log("E fim de semana!");
+} else {
+    console.log("E dia de semana.");
+}
