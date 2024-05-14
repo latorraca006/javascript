c30298b8b959baa635bf57bdce269cba39687b71
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-14 10:04:05 -0400

    adicionado  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.js

---
+++ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.js
@@ -0,0 +1,35 @@
+let textoMultilinha = `
+esta é uma string
+que ocupa múltiplas linhas
+e voce não precisa se preoucupar
+com quebras de linhas ou concatenação
+`;
+console.log(textoMultilinha);
+/*
+Saída:
+"
+Está é uma string
+que ocupa múltiplas linhas
+e voce não precisa se preoucupar
+com quebras de linha ou concatenação.
+"
+*/
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
