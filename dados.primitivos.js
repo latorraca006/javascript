53db9527490bb6eca46e2266748abebc9d5dba85
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-14 09:52:36 -0400

    adicionado dados.primitivos.js

---
+++ dados.primitivos.js
@@ -0,0 +1,13 @@
+let nome = "amanda" // Tipo String
+let verdadeiro = true; // Tipo Boolean
+let falso = false; // tipo Boolean
+let numero = 18; // Tipo Number
+let reais = 25.99; // Tipo Number
+let nulo = null; // Tipo Null
+let indefinido; // Tipo Underfined
+let NotANumber = 0/0; // Tipo NaN
+let objeto = {      //Tipo Object
+    name : "Amanda",
+    age: 18,
+    Profission: "Estudante"
+}
