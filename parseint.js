59218b792eaf9c257518691b27265fa3b12d4064
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-14 10:02:01 -0400

    parseint.js

---
+++ parseint.js
@@ -0,0 +1,8 @@
+let stringParaNumero =  "100"
+console.log(stringParaNumero); //Saida: "100"
+console.log(typeof(stringParaNumero)); // Saida: string
+console.log(""); // Saida: "100"
+//Conversao da String "stringParaNumero" para Number
+let numero = parseInt(stringParaNumero);
+console.log(numero); //Saida: 100
+console.log(typeof(numero)); // Saida: number
