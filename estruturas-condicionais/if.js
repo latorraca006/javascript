bc7e26663f2b0106e1fcead43a8dad88d534244d
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-14 09:39:04 -0400

    adicionado estrutura condicional if

---
+++ estruturas-condicionais/if.js
@@ -0,0 +1,7 @@
+let hora = 11;
+
+if(hora<12) {
+    console.log("Bom dia!");
+}
+// O restante do codigo continua independente se a condicao for verdadeira ou nao
+console.log("Dinheiro nao traz felicidade. Me de o seu e seja feliz!");
