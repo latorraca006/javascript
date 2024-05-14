8ffdfbb34ccafe1934dd0d9649e5065abd1f93e5
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-14 10:00:19 -0400

    concatenacao.2.js

---
+++ concatenacao.2.js
@@ -0,0 +1,4 @@
+let nome = "Amanda";
+let idade = 17;
+let saudacao = "Ola, meu nome e" +amanda+ "e eu tenho" +17+ "anos";
+console.log(saudacao); // Saida: "Ola, meu nome e Amanda e eu tenho 17 anos. "
