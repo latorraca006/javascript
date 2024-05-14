07220d14f9a5d24517e3647b888870cb3cf0e03e
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-14 11:18:51 -0400

    adicionado  funcoes/declaracao-funcao-com-parametro.js

---
+++ funcoes/declaracao-funcao-com-parametro.js
@@ -0,0 +1,7 @@
+function nomeDafuncao(parametro1, parametro2) {
+    // Corpo da funcao
+    // Pode conter uma ou mais instrucoes
+    return resultado; // Opcional
+}
+// Chamada da funcao
+nomeDafuncao(parametro1, parametro2);
