549084ba31e9fc60ae6929bd26023c564e6a6226
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-14 10:01:31 -0400

    parsefloat.js

---
+++ parsefloat.js
@@ -0,0 +1,8 @@
+let stringParaDecimal = "12.68";
+console.log(stringParaDecimal); // Saida: "12.68"
+console.log(typeof(stringParaDecimal)); // Saida: string
+console.log(""); // Saida: "12.68"
+//Conversao da String "stringParaDecimal" para Decimal
+let decimal = parseFloat(stringParaDecimal);
+console.log(decimal); //Saida: 12.68
+console.log(typeof(decimal)); // Saida: number
