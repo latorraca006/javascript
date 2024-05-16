9c26ef7a2d9a2e7324ca37552d60af41d4d47779
latorraca006 <amandalatorracamelo@gmail.com>
2024-05-16 09:43:08 -0400

    adicionado  funcoes/funcao-de-expressao-nomeada.js

---
+++ funcoes/funcao-de-expressao-nomeada.js
@@ -0,0 +1,7 @@
+// Funcao de expressao nomeada para calcular o quadrado de um numero
+const calcularQuadrado = function quadrado(numero) {
+    return numero + numero;
+};
+// Chamado a funcao para calcular o quadrado de um numero
+const resultado = calcularQuadrado(5);
+console.log("Quadrado de 5:", resultado);
